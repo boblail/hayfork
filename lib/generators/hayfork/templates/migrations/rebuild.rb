59c0a0ad832225eba4de86b0224aff0020414ce3
Rob Riebau <Rob.Riebau@cph.org>
2019-02-20 11:04:38 -0600

    [fix] Fixed a bug in the rebuild generator (2 min)
    The `execute` method expects a string, but `<class name>.triggers.rebuild.lines` is an array.

--- lib/generators/hayfork/templates/migrations/rebuild.rb
+++ lib/generators/hayfork/templates/migrations/rebuild.rb
@@ -3,6 +3,6 @@ require "<%= file_name %>_triggers"
 class Rebuild<%= table_name.camelize %> < ActiveRecord::Migration<%= migration_version %>
   def up
     execute <%= class_name %>.triggers.replace
-    execute <%= class_name %>.triggers.rebuild.lines
+    execute <%= class_name %>.triggers.rebuild
   end
 end
